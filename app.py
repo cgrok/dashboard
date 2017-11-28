ae96c6ce8a17e36545b3bac247aafb33d1ef4e97
verixx <abdurraqeeb53@gmail.com>
2017-11-29 07:29:44 +1100

    Supporting heroku with $PORT

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from sanic import Sanic
-from sanic.response import text
+from sanic.response import html
+import os
 
 app = Sanic(__name__)
 
 @app.route('/')
 async def index(request):
-    print(request.ip)
-    return text("Hello World")
+    return html("<h1>Kyber was here</h1>")
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=6969)
+    app.run(port=os.getenv('$PORT'))
