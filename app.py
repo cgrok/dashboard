843c2bd67fc6b8c08306fba417baf973656552d3
verixx <abdurraqeeb53@gmail.com>
2017-11-29 08:06:33 +1100

    Forgot a comma

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ async def index(request):
     return html("<h1>Kyber was here</h1>")
 
 if __name__ == '__main__':
-    print(os.environ)
     app.run(
-        port=int(os.getenv('PORT', 8000))
+        port=int(os.getenv('PORT', 8000)),
         workers=int(os.getenv('WEB_CONCURRENCY', 1))
         )
