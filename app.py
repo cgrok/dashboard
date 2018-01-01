bc03a3be3d3d0cae1a56fd2cb575dfeae6b078ad
verixx <abdurraqeeb53@gmail.com>
2018-01-02 10:29:46 +1100

    Got the discord authentication flow working

--- app.py
+++ app.py
@@ -22,18 +22,42 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-from sanic import Sanic
-from sanic.response import html, text, HTTPResponse
-from motor.motor_asyncio import AsyncIOMotorClient
-import aiohttp
 import os
+import hmac
+import hashlib
+from functools import wraps
+
+from sanic import Sanic
+from sanic.response import html, text, redirect, HTTPResponse
+from sanic_session import InMemorySessionInterface
+from motor.motor_asyncio import AsyncIOMotorClient
+from urllib.parse import urlencode
+import aiohttp
 import discord
 import asyncio
 import ujson
-import hmac
-import hashlib
+
+with open('data/config.json') as f:
+    CONFIG = ujson.loads(f.read())
+
+OAUTH2_CLIENT_ID = CONFIG.get('client_id')
+OAUTH2_CLIENT_SECRET = CONFIG.get('client_secret')
+OAUTH2_REDIRECT_URI = 'http://127.0.0.1:8000/callback'
+
+API_BASE_URL = 'https://discordapp.com/api'
+AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
 
 app = Sanic('dash')
+session_interface = InMemorySessionInterface()
+
+@app.middleware('request')
+async def add_session_to_request(request):
+    await session_interface.open(request)
+
+@app.middleware('response')
+async def save_session(request, response):
+    await session_interface.save(request, response)
 
 def json(data, status=200, headers=None):
     return HTTPResponse(
@@ -43,45 +67,51 @@ def json(data, status=200, headers=None):
         content_type='application/json'
         )
 
-def authrequired():
-    def decorator(f):
+async def validate_token(request):
+    exists = await app.db.admin.find_one({'token': request.token})
+    return exists is not None
+    
+def authrequired(admin=False):
+    def decorator(func):
+        @wraps(func)
         async def wrapper(request, *args, **kwargs):
-            token = request.headers.get('Authorization')
-            if not token:
-                return error('Missing authentication key.')
-            if not token.startswith('Bearer'):
-                return error('Invalid authentication key provided.')
-            exists = await app.db.admin.find_one({'token': token.replace('Bearer ', '')})
-            if exists is not None:
-                return await f(request, *args, **kwargs)
+            valid_token = await validate_token(request)
+            if valid_token:
+                return await func(request, *args, **kwargs)
+            if admin is False and not request['session'].get('logged_in'):
+                return text('You need to be logged in to use this endpoint.')
             else:
-                return error('Invalid authentication key provided.')
+                return await func(request, *args, **kwargs)
+            if admin is True and not valid_token:
+                return error('Invalid authorization token provided.')
         return wrapper
     return decorator
 
 @app.listener('before_server_start')
 async def init(app, loop):
     app.session = aiohttp.ClientSession(loop=loop)
-    with open('data/config.json') as f:
-        data = ujson.loads(f.read())
-        app.password = data.get('password')
-        app.webhook_url = data.get('webhook_url')
-        app.log_url = data.get('log_url')
-        mongo_client = AsyncIOMotorClient(data.get('mongo_url'))
-        app.db = mongo_client.dash
-    
+    app.password = CONFIG.get('password')
+    app.webhook_url = CONFIG.get('webhook_url')
+    app.log_url = CONFIG.get('log_url')
+    mongo_client = AsyncIOMotorClient(CONFIG.get('mongo_url'))
+    app.db = mongo_client.dash
+
     if app.webhook_url:
         await app.session.post(
             app.webhook_url, 
             json=format_embed('deploy')
             )
- 
-@app.route('/')
+
+@app.listener('after_server_stop')
+async def aexit(app, loop):
+    app.session.close()
+
+@app.get('/')
 async def index(request):
-    with open('build/html')
-    return html('This is meant to be a bot dashboard website')
+    return text('This is meant to be an index.html')
 
 @app.get('/api/bots/<bot_id:int>')
+@authrequired()
 async def get_bot_info(request, bot_id):
     data = await app.db.bot_info.find_one({"bot_id": bot_id})
     if not data:
@@ -92,14 +122,67 @@ async def get_bot_info(request, bot_id):
     return json(data)
 
 @app.post('/api/bots/<bot_id:int>')
-@authrequired()
+@authrequired(admin=True)
 async def set_bot_info(request, bot_id):
     data = request.json
     await app.db.bot_info.update_one(
         {"bot_id": bot_id},
         {"$set": data}, upsert=True
-    )
+        )
     return json({'success': True})
+
+@app.get('/login')
+async def login(request):
+    data = {
+        "scope": "identify guilds",
+        "client_id": OAUTH2_CLIENT_ID,
+        "response_type": "code"
+    }
+    return redirect(f"{AUTHORIZATION_BASE_URL}?{urlencode(data)}")
+
+@app.get('/callback')
+async def oauth_callback(request):
+    code = request.raw_args.get('code')
+    token = await fetch_token(code)
+    request['session']['logged_in'] = True
+    request['session']['discord_token'] = token['access_token']
+    return redirect(app.url_for('profile'))
+
+@app.get('/logout') 
+@authrequired()
+async def logout(request):
+    request['session'].clear()
+    return text('Logged out!')
+
+@app.get('/profile')
+@authrequired()
+async def profile(request):
+    token = request['session']['discord_token']
+    headers = {"Authorization": f"Bearer {token}"}
+    user = await app.session.get(f"{API_BASE_URL}/users/@me", headers=headers)
+    guilds = await app.session.get(f"{API_BASE_URL}/users/@me/guilds", headers=headers)
+    return json((dict(user=await user.json(), guilds=await guilds.json())))
+
+@app.post('/hooks/github')
+async def upgrade(request):
+    if not validate_payload(request):
+        return error()
+    app.loop.create_task(restart_later())
+    return json({'success': True})
+
+async def fetch_token(code):
+    data = {
+        "code": code,
+        "grant_type": "authorization_code",
+        "redirect_uri": OAUTH2_REDIRECT_URI,
+        "client_id": OAUTH2_CLIENT_ID,
+        "client_secret": OAUTH2_CLIENT_SECRET
+    }
+
+    async with app.session.post(f"{TOKEN_URL}?{urlencode(data)}") as resp:
+        json = await resp.json()
+        print(json)
+        return json
 
 def format_embed(event):
     event = event.lower()
@@ -152,16 +235,9 @@ def error(reason, status=401):
         "message": reason
         }, status=status)
 
-@app.post('/hooks/github')
-async def upgrade(request):
-    if not validate_payload(request):
-        return error()
-    app.loop.create_task(restart_later())
-    return json({'success': True})
-
 if __name__ == '__main__':
     if os.getenv('VSCODE_PID'): # not on vps 
-        app.run(host='localhost', port=8000)
+        app.run()
     else: 
         app.run(host='0.0.0.0', port=80)
 
